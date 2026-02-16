7e061d8a5406969ec2c0580f1991d5d4d5d6a50a
Ido Bahir <idobahir@gmail.com>
2026-02-17 00:55:44 +0200

    Restore

--- run_monitor.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "C:\Users\Ido Bahir\Documents\Gemini\SneakerMonitor"
-echo Installing dependencies...
-call npm install
-echo Starting SneakerMonitor...
-node src/monitor.js
-echo.
-echo Monitor finished (or crashed). Press any key to close.
-pause
